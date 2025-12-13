f74ab5e853d6e8a3463b6a5631743bcd01161c01
mayyan531 <mayyan531@gmail.com>
2025-12-13 02:04:20 -0500

    day four part 2

--- Day_Four.py
+++ Day_Four.py
@@ -49,5 +49,68 @@ def Part_One():
             #print(total)
     print(total)
 
-Part_One()
+def Part_Two():
+    total = 0
+    lanes = []
+    removed = []
+    with open(input_file, 'r') as file:
+        input = file.readlines()
+
+    for lane in input:
+        lanes.append(list(lane.strip()))
+
+    while True:
+        #print(lanes)
+        for num, lane in enumerate(lanes):
+            length = len(lane)
+            #print("\nchecking for " + lane)
+
+            for idx, roll in enumerate(lane):
+                if roll != "@":
+                    continue;
+                
+                total += 1
+                idx_to_check = []
+                roll_adj = 0
+                
+                neighbors = [
+                    (num-1, idx-1),
+                    (num-1, idx),
+                    (num-1, idx+1),
+                    (num,   idx-1),
+                    (num,   idx+1),
+                    (num+1, idx-1),
+                    (num+1, idx),
+                    (num+1, idx+1)
+                ]
+
+                for r, c in neighbors:
+                    if (r < 0 or c < 0 or r>= len(lanes) or c>= length):
+                        continue
+                    
+                    idx_to_check.append(lanes[r][c])
+                
+                #print("index " + str(idx))
+                #print("checking " + str(idx_to_check))
+                for index in idx_to_check:
+                    if index == "@":
+                        roll_adj += 1
+
+                    if roll_adj == 4:
+                        total -= 1
+                        break;
+                
+                if roll_adj != 4:
+                    removed.append((num, idx))
+
+        if len(removed) == 0:
+            break
+        else:
+            for r,c in removed: lanes[r][c] = "."
+            #print("lanes after a pass" + str(lanes))
+        removed=[]
+    print(total)
+
+
+Part_Two()
 
